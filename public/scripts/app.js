d8e0e6736e06be68ca4b78bcd03d89c71c63034b
eltimsy <timothy.sc.tse@gmail.com>
2016-09-20 21:21:06 -0400

    added validation and post and get functions to app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,56 +4,6 @@ $(document).ready(function() {
    * jQuery is already loaded
    * Reminder: Use (and do all your DOM work in) jQuery's document ready function
    */
-   var data = [
-     {
-       "user": {
-         "name": "Newton",
-         "avatars": {
-           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-         },
-         "handle": "@SirIsaac"
-       },
-       "content": {
-         "text": "If I have seen further it is by standing on the shoulders of giants"
-       },
-       "created_at": 1461116232227
-     },
-     {
-       "user": {
-         "name": "Descartes",
-         "avatars": {
-           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-         },
-         "handle": "@rd" },
-       "content": {
-         "text": "Je pense , donc je suis"
-       },
-       "created_at": 1461113959088
-     },
-     {
-       "user": {
-         "name": "Johann von Goethe",
-         "avatars": {
-           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-         },
-         "handle": "@johann49"
-       },
-       "content": {
-         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-       },
-       "created_at": 1461113796368
-     }
-   ];
-
-  function renderTweets(tweets) {
-
-  }
   function createTweetHeader(header){
     var $newHeader = $("<header>");
     var $newHtwo = $("<h2>");
@@ -87,9 +37,10 @@ $(document).ready(function() {
     var $newIretweet = $("<i>").addClass("fa fa-retweet");
     var $newIheart = $("<i>").addClass("fa fa-heart");
     var $newNodeText = $(document.createTextNode("Just tweeted"));
+    var theDay = Math.floor((Date.now() - footer)/86400000);
 
     $newDivTime.attr("data-time",footer);
-    $newDivTime.append($newNodeText);
+    $newDivTime.append(theDay + " days ago.");
     $newDivSocial.append($newIFlag, $newIretweet, $newIheart);
     $newFooter.append($newDivTime, $newDivSocial);
     return $newFooter
@@ -100,13 +51,11 @@ $(document).ready(function() {
     var $user = [tweet.user.name, tweet.user.handle, tweet.user.avatars.small];
     var $content = [tweet.content.text]
     var $createdAt = [tweet.created_at]
-    console.log($createdAt)
     var $footer = createFooter($createdAt);
     var $paragraph = createParagraph($content);
     var $header = createTweetHeader($user);
-
-    console.log($header);
     var $handle = tweet.handle;
+
     $tweet.append($header, $paragraph, $footer);
     return $tweet;
   }
@@ -114,11 +63,49 @@ $(document).ready(function() {
   function renderTweets(tweets) {
     for(var data of tweets) {
       var $tweet = createTweetElement(data);
-      console.log($tweet);
       $("#all-tweets").append($tweet);
     }
   }
 
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function(data) {
+        renderTweets(data)
+      }
+    });
+  }
 
+  loadTweets();
+
+
+  $(".tweet-form").on("submit", function(env){
+    env.preventDefault();
+    var textTotal = $('.tweet-text').val().length;
+    var tweetData = $(this).serialize();
+
+    $('.error-message').text('');
+    $('.error-message').fadeIn();
+    if(textTotal === 0) {
+      console.log($('.error-message'));
+      $('.error-message').text("Type something please");
+      $('.error-message').fadeOut(1500);
+    } else if(textTotal > 140) {
+      $('.error-message').text("You typed more than 140 characters!");
+      $('.error-message').fadeOut(1500);
+    } else {
+      $.ajax({
+        url: '/tweets',
+        method: 'post',
+        data: tweetData,
+        //dataType: String
+        success: function(data){
+          console.log(data);
+          var $tweet = createTweetElement(data);
+          $("#all-tweets").append($tweet);
+        }
+      });
+    }
+  });
 });
